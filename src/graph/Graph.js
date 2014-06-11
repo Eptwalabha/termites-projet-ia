85175804727808f595566241ba16dfdd701984c1
Eptwalabha <damiengabrielle@gmail.com>
2014-06-11 15:40:21 +0200

    Correction des collisions entre segments
    Adding two "test" html files.

--- src/graph/Graph.js
+++ src/graph/Graph.js
@@ -29,3 +29,62 @@ Graph.prototype.generateEdges = function() {
         }
     }
 };
+
+Graph.prototype.randomize = function(walls, canvasElement) {
+
+    var width = canvasElement.width / 6;
+    var height = canvasElement.height / 6;
+    this.vertices = [];
+    for (var i = 0; i < 7; i++) {
+        for (var j = 0; j < 7; j++) {
+            var noCollide = true;
+            var vertex = new Vertex(width * i, height * j);
+            for (var k = 0, size2 = walls.length; k < size2; k++) {
+                if (isPointInWall(vertex, walls[k])) {
+                    noCollide = false;
+                    break;
+                }
+            }
+            if (noCollide)
+                this.vertices.push(vertex);
+        }
+    }
+    this.generateEdges();
+    var edgeList = this.edges;
+    this.edges = [];
+
+    console.log("edgelist = " + edgeList.length);
+    console.log("edge = " + this.edges.length);
+    console.log("walls = " + walls.length);
+
+    for (var j = 0, size = edgeList.length; j < size; j++) {
+        var insert = true;
+        var edgeToTest = edgeList[j];
+        for (k = 0, size2 = walls.length; k < size2; k++) {
+            if (doEdgeIntersectsWall(edgeToTest, walls[k])) {
+                insert = false;
+                break;
+            }
+        }
+        if (insert) {
+            this.edges.push(edgeToTest);
+        }
+    }
+
+    console.log("edge counter= " + this.edges.length);
+};
+
+Graph.prototype.draw = function(context) {
+    context.strokeStyle="#001";
+    for (var i = 0, verticesSize = this.vertices.length; i < verticesSize; i++) {
+        context.beginPath();
+        context.arc(this.vertices[i].x, this.vertices[i].y, 4, 0, 2*Math.PI);
+        context.stroke();
+    }
+    for (var j = 0, edgesSize = this.edges.length; j < edgesSize; j++) {
+        context.beginPath();
+        context.moveTo(this.edges[j].a.x, this.edges[j].a.y);
+        context.lineTo(this.edges[j].b.x, this.edges[j].b.y);
+        context.stroke();
+    }
+};
